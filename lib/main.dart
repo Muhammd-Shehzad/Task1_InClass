a84b3795778c7a44a76d58851264f2f56e1cf4d1
Muhammd-Shehzad <127086042+Muhammd-Shehzad@users.noreply.github.com>
2024-06-14 23:35:33 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+      body: Container(
+        height: 100,
+        width: 200,
+        color: Colors.white,
+
+        child: Column(),
+
+
+
+
+
+      ),
+
+
+
+
+
+
+      ),
+    ),
+  );
+}
